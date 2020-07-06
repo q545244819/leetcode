fec28204dc3d4725c31f0202a36833374c49c422
john <545244819@qq.com>
2020-07-06 23:15:00 +0800

    修改面试题 40 代码

--- 剑指offer/40/main2.cpp
+++ 剑指offer/40/main2.cpp
@@ -2,10 +2,18 @@ class Solution {
 public:
     vector<int> getLeastNumbers(vector<int>& arr, int k) {
         vector<int> ans;
+        if (k == 0) return ans;
         priority_queue<int> PQ;
-        for (int i = 0; i < arr.size(); i++) PQ.push(-1 * arr[i]);
+        for (int i = 0; i < arr.size(); i++) {
+            if (PQ.size() == k && PQ.top() > arr[i]) {
+                PQ.pop();
+                PQ.push(arr[i]);
+            } else if (PQ.size() < k) {
+                PQ.push(arr[i]);
+            }
+        }
         for (int i = 0; i < k; i++) {
-            ans.push_back(-1 * PQ.top());
+            ans.push_back(PQ.top());
             PQ.pop();
         }
         return ans;
