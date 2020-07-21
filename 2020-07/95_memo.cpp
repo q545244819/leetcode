0be1d3314fcfef3f29d0c3a9d636b71f4c329bb7
john <545244819@qq.com>
2020-07-21 10:41:28 +0800

    打卡题 95 记忆化搜索版本

---
+++ 2020-07/95_memo.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+const int N = 9;
+vector<vector<vector<TreeNode*>>> memo(N, vector<vector<TreeNode*>>(N));
+
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        if (n == 0) return {};
+        return dfs(1, n);
+    }
+
+    vector<TreeNode*> dfs (int l, int r) {
+        if (l > r) return { NULL };
+        if (l == r) {
+            TreeNode* node = new TreeNode(l);
+            return memo[l][r] = { node };
+        }
+        if (memo[l][r].size()) return memo[l][r];
+
+        vector<TreeNode*> res;
+
+        for (int i = l; i <= r; i++) {
+            vector<TreeNode*> lefts = dfs(l, i - 1);
+            vector<TreeNode*> rights = dfs(i + 1, r);
+
+            for (TreeNode* left: lefts) {
+                for (TreeNode* right: rights) {
+                    TreeNode* node = new TreeNode(i, left, right);
+                    res.push_back(node);
+                }
+            }
+        }
+
+        return memo[l][r] = res;
+    }
+};
